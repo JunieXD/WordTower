1ebe894953b9aae4bd952f8fddec010e4eebecad
JunieXD <1240840076@qq.com>
2025-11-02 22:53:42 +0800

    开始搭建框架

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <n-layout has-sider>
+    <!-- 侧边栏：仅桌面端显示 -->
+    <n-layout-sider> sider </n-layout-sider>
+
+    <!-- 主布局（Header + Content + Footer） -->
+    <n-layout>
+      <n-layout-header> header </n-layout-header>
+
+      <n-layout-content>
+        <router-view />
+      </n-layout-content>
+
+      <n-layout-footer> footer </n-layout-footer>
+    </n-layout>
+  </n-layout>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="text-red-500 text-2xl">Home Content</div>
 </template>
