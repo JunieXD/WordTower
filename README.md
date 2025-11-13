b4c8cb3fe9529b1ced6b9e1c5a129e2f82992373
JunieXD <1240840076@qq.com>
2025-11-14 01:28:25 +0800

    去除了init_db脚本，用alembic进行数据库版本管理

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
-# 运行项目
+# WordTower
 
 ## 开发环境
 
+### 数据库迁移
+
 ```
-uvicorn app.main:app --reload
+cd app
+uv run alembic upgrade head
+```
+
+### 运行项目
+
+```
+uv run uvicorn app.main:app --reload
 ```
--- app/db/database.py
+++ app/db/database.py
@@ -10,7 +10,4 @@ def get_session():
     with Session(engine) as session:
         yield session
 
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
-
 SessionDep = Annotated[Session, Depends(get_session)]
