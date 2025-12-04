35999051009af44d3548874a3eabd07a2b351263
JunieXD <1240840076@qq.com>
2025-12-05 01:19:56 +0800

    修改了题目生成概率

--- app/utils/config.py
+++ app/utils/config.py
@@ -3,7 +3,7 @@ import random
 
 def get_question_type_weights(floor: int) -> list[int]:
     """根据楼层返回题目类型权重"""
-    return [100 - min(floor // 10 * 2, 20), min(floor // 10, 10), min(floor // 10, 10)]
+    return [100 - min(floor / 10 * 4, 20), min(floor / 10 * 2, 10), min(floor / 10 * 2, 10)]
     # return [0, 100, 0]
 
 def get_enemy_hp(floor: int) -> int:
