f94e3ed645375457d3f10b3e47f8d61a49a4a1c3
JunieXD <1240840076@qq.com>
2025-12-11 01:43:12 +0800

    添加PWA安装；暂时移除了排行榜

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import tailwindcss from '@tailwindcss/vite'
-// import fs from 'fs'
-// import path from 'path'
-
-// const keyPath = path.resolve(__dirname, 'localhost+3-key.pem')
-// const certPath = path.resolve(__dirname, 'localhost+3.pem')
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -19,64 +15,64 @@ export default defineConfig({
     AutoImport({
       imports: ['vue'],
     }),
-    // VitePWA({
-    //   registerType: 'autoUpdate',
-    //   includeAssets: ['WTlogo.ico', 'WTlogo.png', 'WTlogo.svg', 'WTlogo-192.png', 'WTlogo-512.png'],
-    //   manifest: {
-    //     name: 'Word Tower',
-    //     short_name: 'WordTower',
-    //     description: 'Word Tower',
-    //     theme_color: '#ffffff',
-    //     background_color: '#ffffff',
-    //     display: 'minimal-ui',
-    //     orientation: 'portrait',
-    //     scope: '/',
-    //     start_url: '/',
-    //     icons: [
-    //       {
-    //         src: '/WTlogo-192.png',
-    //         sizes: '192x192',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: '/WTlogo-512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //       },
-    //     ],
-    //   },
-    //   workbox: {
-    //     globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
-    //     runtimeCaching: [
-    //       {
-    //         urlPattern: /^https:\/\/.*\.(?:png|jpg|jpeg|svg|gif|webp)$/i,
-    //         handler: 'CacheFirst',
-    //         options: {
-    //           cacheName: 'images-cache',
-    //           expiration: {
-    //             maxEntries: 50,
-    //             maxAgeSeconds: 60 * 60 * 24 * 30, // 30天
-    //           },
-    //         },
-    //       },
-    //       {
-    //         urlPattern: /^https:\/\/.*\/api\/.*/i,
-    //         handler: 'NetworkFirst',
-    //         options: {
-    //           cacheName: 'api-cache',
-    //           expiration: {
-    //             maxEntries: 50,
-    //             maxAgeSeconds: 60 * 5, // 5分钟
-    //           },
-    //           networkTimeoutSeconds: 10,
-    //         },
-    //       },
-    //     ],
-    //   },
-    //   devOptions: {
-    //     enabled: true,
-    //   },
-    // }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['WTlogo.ico', 'WTlogo.png', 'WTlogo.svg', 'WTlogo-192.png', 'WTlogo-512.png'],
+      manifest: {
+        name: 'Word Tower',
+        short_name: 'WordTower',
+        description: 'Word Tower',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'minimal-ui',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: '/WTlogo-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/WTlogo-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/.*\.(?:png|jpg|jpeg|svg|gif|webp)$/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'images-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30天
+              },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/.*\/api\/.*/i,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'api-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 5, // 5分钟
+              },
+              networkTimeoutSeconds: 10,
+            },
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
   ],
   resolve: {
     alias: {
